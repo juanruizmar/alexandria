5281fa1d2b8b22657c1261c78311f51a2870da41
Juan <jruizmartos@gmail.com>
2025-01-26 16:24:18 +0100

    histogram interface standardized

--- data_build/histogram.cpp
+++ data_build/histogram.cpp
@@ -1,5 +1,5 @@
 #include "histogram.hpp"
 
-inline void histogram::print(std::ostream &os){
+inline void histogram::display(std::ostream &os){
     for(int i=0; i<n_segments(); ++i) os << frequencies_[i] << " " << min_+i*segment_length() << " " << max_-(n_segments()-i-1)*segment_length() << std::endl;
 }
