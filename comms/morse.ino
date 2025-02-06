e7f6f33c40fadf8de37bb7fca0c4169085b9438a
Juan <jruizmartos@gmail.com>
2025-02-06 02:27:46 +0100

    bug fixed

--- comms/morse.ino
+++ comms/morse.ino
@@ -9,6 +9,18 @@ int tokenSeparatorLength;
 int charSeparatorLength;
 int wordSeparatorLength;
 
+#define IS_A_LETTER(c) ('a'<=c && c<='z')
+#define GET_INDEX_AS_A_LETTER(c) (c-'a')
+
+#define IS_A_DIGIT_OR_CLOSE(c) ('&'<=c && c<='9')
+#define GET_INDEX_AS_A_DIGIT_OR_CLOSE(c) (c-'&')
+
+#define IS_A_LETTER_WITH_ACCENT(c) ('ü'<=c && c<='ì')
+#define GET_INDEX_AS_LETTER_WITH_ACCENT(c) (c-'ü')
+
+#define IS_UPPER_CASE(c) ('A'<=c && c<='Z')
+#define AS_LOWER_CASE(c) (c+='a'-'A')
+
 #define TURN_LIGHT_ON digitalWrite(13, HIGH);
 #define TURN_LIGHT_OFF digitalWrite(13, LOW);
 
