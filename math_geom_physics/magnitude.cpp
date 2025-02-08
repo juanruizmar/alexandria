e66a76b48fd2602167eb6cf5f2cf989a456ad9ce
Juan <jruizmartos@gmail.com>
2025-02-08 22:27:09 +0100

    improved functionalities of magnitudes and units

--- math_geom_physics/magnitude.cpp
+++ math_geom_physics/magnitude.cpp
@@ -1,139 +1,128 @@
 #include "magnitude.hpp"
 
+#include <iostream>
+
 using namespace std;
 
-magnitude magnitude::distance(magnitudeSymbols::DISTANCE);
-magnitude magnitude::mass(magnitudeSymbols::MASS);
-magnitude magnitude::time(magnitudeSymbols::TIME);
-magnitude magnitude::current(magnitudeSymbols::CURRENT);
-magnitude magnitude::temperature(magnitudeSymbols::TEMPERATURE);
-magnitude magnitude::substanceAmount(magnitudeSymbols::SUBSTANCE_AMOUNT);
-magnitude magnitude::luminosity(magnitudeSymbols::LUMINOSITY);
-magnitude magnitude::rotation(magnitudeSymbols::ROTATION);
+magnitude magnitude::distance(DISTANCE, "distance", "d", "metter", "m");
+magnitude magnitude::mass(MASS, "mass", "m", "kilogram", "Kg");
+magnitude magnitude::time(TIME, "time", "t", "second", "s");
+magnitude magnitude::current(CURRENT, "current", "I", "ampere", "A");
+magnitude magnitude::temperature(TEMPERATURE, "temperature", "T", "kelvin", "K");
+magnitude magnitude::substanceAmount(SUBSTANCE_AMOUNT, "amount of substance", "n", "mol", "mol");
+magnitude magnitude::luminosity(LUMINOSITY, "luminosity", "I_V", "candela", "cd");
+magnitude magnitude::rotation(ROTATION, "rotation", "theta", "pi*radian", "(pi*rads)");
 
-magnitude::magnitudeSymbols::magnitudeSymbols(){
-    basicMagnitudesSymbols[DISTANCE] = "l";
-    basicMagnitudesSymbols[MASS] = "m";
-    basicMagnitudesSymbols[TIME] = "t";
-    basicMagnitudesSymbols[CURRENT] = "i";
-    basicMagnitudesSymbols[TEMPERATURE] = "T";
-    basicMagnitudesSymbols[SUBSTANCE_AMOUNT] = "n";
-    basicMagnitudesSymbols[LUMINOSITY] = "I_V";
-    basicMagnitudesSymbols[ROTATION] = "phi";
-    
-    basicUnitsSymbols[DISTANCE] = "m";
-    basicUnitsSymbols[MASS] = "Kg";
-    basicUnitsSymbols[TIME] = "s";
-    basicUnitsSymbols[CURRENT] = "A";
-    basicUnitsSymbols[TEMPERATURE] = "K";
-    basicUnitsSymbols[SUBSTANCE_AMOUNT] = "mol";
-    basicUnitsSymbols[LUMINOSITY] = "cd";
-    basicUnitsSymbols[ROTATION] = "(pi*rads)";
-}
+magnitude magnitude::area(distance.square(), "area", "A");
+magnitude magnitude::volume(distance.cube(), "volume", "V");
+magnitude magnitude::frequency(time.inverse(), "frequency", "f", "herz", "Hz");
+magnitude magnitude::velocity(distance/time, "velocity", "v");
+magnitude magnitude::acceleration(velocity/time, "acceleration", "a");
+magnitude magnitude::angularVelocity(rotation/time, "angular velocity", "omega");
+magnitude magnitude::density(mass/volume, "density", "rho");
+magnitude magnitude::force(mass*acceleration, "force", "F");
+magnitude magnitude::pressure(force/area, "pressure", "P");
+magnitude magnitude::energy(force*distance, "energy", "E");
+magnitude magnitude::power(energy/time, "power", "P");
+magnitude magnitude::momentum(mass*velocity, "momentum", "p");
+magnitude magnitude::charge(current*time, "charge", "q");
+magnitude magnitude::work(distance*force, "work", "W");
+magnitude magnitude::heat(mass*velocity.square(), "heat", "Q");
+magnitude magnitude::entropy(heat/temperature, "entropy", "S");
+magnitude magnitude::voltage(energy/charge, "voltage", "V");
+magnitude magnitude::resistance(voltage/current, "resistance", "R");
+magnitude magnitude::impedance(voltage/current, "impedance", "Z");
+magnitude magnitude::capacitance(charge/voltage, "capacitance", "C");
 
-magnitude::magnitudeSymbols magnitude::symbols;
+magnitude magnitude::electricField(force/charge, "electric field", "boldE");
+magnitude magnitude::magneticField(electricField/velocity, "magnetic field", "boldB");
+magnitude magnitude::electricFlux(electricField*area, "electric flux", "Phi_E");
+magnitude magnitude::magneticFlux(magneticField*area, "magnetic flux", "Phi_B");
 
-magnitude magnitude::area(distance.square());
-magnitude magnitude::volume(distance.cube());
-magnitude magnitude::density(mass/volume);
-magnitude magnitude::velocity(distance/time);
-magnitude magnitude::acceleration(velocity/time);
-magnitude magnitude::force(mass*acceleration);
-magnitude magnitude::pressure(force/area);
-magnitude magnitude::energy(force*distance);
-magnitude magnitude::power(energy/time);
-magnitude magnitude::frequency(time.inverse());
-magnitude magnitude::charge(current*time);
-magnitude magnitude::voltage(energy/charge);
-magnitude magnitude::resistance(voltage/current);
-magnitude magnitude::capacitance(charge/voltage);
-magnitude magnitude::inductance(magneticFlux/current);
-magnitude magnitude::momentum(mass*velocity);
-magnitude magnitude::angularVelocity(rotation/time);
-magnitude magnitude::work(distance*force);
-magnitude magnitude::heat(mass*velocity.square());
-magnitude magnitude::entropy(heat/temperature);
-magnitude magnitude::impedance(voltage/current);
+magnitude magnitude::inductance(magneticFlux/current, "inductance", "L");   // Defined after magneticFlux
 
-magnitude magnitude::electricField(force/charge);
-magnitude magnitude::electricFlux(electricField*area);
-magnitude magnitude::magneticField(electricField/velocity);
-magnitude magnitude::magneticFlux(magneticField*area);
-
-string magnitude::symbol() const{
-    string res;
-    for(int i=0; i<magnitudeSymbols::nBasicComponents; ++i) if(exponents[i]!=0){
-        if(res.size()!=0) res += " * ";
-        if(exponents[i]==1) res += symbols.basicMagnitudesSymbols[i];
-        else res += symbols.basicMagnitudesSymbols[i] + "^" + to_string(exponents[i]);
-    }
+magnitude magnitude::operator *(const magnitude &other) const{
+    magnitude res(payload()*other.payload());
+    transform(exponents.begin(), exponents.end(),  other.exponents.begin(), res.exponents.begin(), [](char a, char b) { return a+b; });
     return res;
 }
-string magnitude::units() const{
-    string res;
-    for(int i=0; i<magnitudeSymbols::nBasicComponents; ++i) if(exponents[i]!=0){
-        if(res.size()!=0) res += " * ";
-        if(exponents[i]==1) res += symbols.basicUnitsSymbols[i];
-        else res += symbols.basicUnitsSymbols[i] + "^" + to_string(exponents[i]);
-    }
-    return res;
-}
-
-magnitude magnitude::operator *(const magnitude &m) const{
-    magnitude res;
-    transform(exponents.begin(), exponents.end(),  m.exponents.begin(), res.exponents.begin(), [](char a, char b) { return a+b; });
-    return res;
-}
-magnitude magnitude::operator /(const magnitude &m) const{
-    magnitude res;
-    transform(exponents.begin(), exponents.end(),  m.exponents.begin(), res.exponents.begin(), [](char a, char b) { return a-b; });
+magnitude magnitude::operator /(const magnitude &other) const{
+    magnitude res(payload()/other.payload());
+    transform(exponents.begin(), exponents.end(),  other.exponents.begin(), res.exponents.begin(), [](char a, char b) { return a-b; });
     return res;
 }
 
 magnitude magnitude::square() const{
-    magnitude res;
+    magnitude res(payload().square());
     transform(exponents.begin(), exponents.end(), res.exponents.begin(), [](char n) { return n*2; });
     return res;
 }
 magnitude magnitude::cube() const{
-    magnitude res;
+    magnitude res(payload().cube());
     transform(exponents.begin(), exponents.end(), res.exponents.begin(), [](char n) { return n*3; });
     return res;
 }
 magnitude magnitude::inverse() const{
-    magnitude res;
+    magnitude res(payload().inverse());
     transform(exponents.begin(), exponents.end(), res.exponents.begin(), [](char n) { return -n; });
     return res;
 }
 
-template<unsigned int N> vectorialVariable<N> vectorialVariable<N>::operator +(const vectorialVariable<N> &v) const{
-    vectorialVariable<N> res;
-    transform(components.begin(), components.end(), v.components.begin(), res.components.begin(), [](double a, double b) { return a+b; });
+opperation_defined_name_and_symbol user_defined_name_and_symbol::square() const{
+    return map<user_defined_name_and_symbol, float>{std::pair(*this, 2)};
+}
+opperation_defined_name_and_symbol user_defined_name_and_symbol::cube() const{
+    return map<user_defined_name_and_symbol, float>{std::pair(*this, 3)};
+}
+opperation_defined_name_and_symbol user_defined_name_and_symbol::inverse() const{
+    return map<user_defined_name_and_symbol, float>{std::pair(*this, -1)};
+}
+opperation_defined_name_and_symbol opperation_defined_name_and_symbol::square() const{
+    opperation_defined_name_and_symbol res(*this);
+    for(auto &i: res.map) i.second*=2;
     return res;
 }
-template<unsigned int N> vectorialVariable<N> vectorialVariable<N>::operator -(const vectorialVariable<N> &v) const{
-    vectorialVariable<N> res;
-    transform(components.begin(), components.end(), v.components.begin(), res.components.begin(), [](double a, double b) { return a-b; });
+opperation_defined_name_and_symbol opperation_defined_name_and_symbol::cube() const{
+    opperation_defined_name_and_symbol res(*this);
+    for(auto &i: res.map) i.second*=3;
     return res;
 }
-template<unsigned int N> vectorialVariable<N> vectorialVariable<N>::operator *(const vectorialVariable<N> &v) const{
-    vectorialVariable<N> res;
-    transform(components.begin(), components.end(), v.components.begin(), res.components.begin(), [](double a, double b) { return a*b; });
-    return res;
-}
-template<unsigned int N> vectorialVariable<N> vectorialVariable<N>::operator /(const vectorialVariable<N> &v) const{
-    vectorialVariable<N> res;
-    transform(components.begin(), components.end(), v.components.begin(), res.components.begin(), [](double a, double b) { return a/b; });
-    return res;
-}
-template<unsigned int N> vectorialVariable<N> vectorialVariable<N>::operator -() const{
-    vectorialVariable<N> res;
-    transform(components.begin(), components.end(), res.components.begin(), [](double n) { return -n; });
+opperation_defined_name_and_symbol opperation_defined_name_and_symbol::inverse() const{
+    opperation_defined_name_and_symbol res(*this);
+    for(auto &i: res.map) i.second*=-1;
     return res;
 }
 
-vectorialVariable<3> crossProduct(const vectorialVariable<3> &v1, const vectorialVariable<3> &v2){
-    vectorialVariable<3> res(v1.unit_*v2.unit_);
-    for(int i=0; i<3; ++i) res.components[i%3] = v1.components[(i+1)%3] * v2.components[(i+2)%3] - v1.components[(i+2)%3] * v2.components[(i+1)%3];
+opperation_defined_name_and_symbol name_and_symbol::operator *(const name_and_symbol &other) const{
+    return opperation_defined_name_and_symbol(as_raw_map(), other.as_raw_map());
+}
+opperation_defined_name_and_symbol name_and_symbol::operator /(const name_and_symbol &other) const{
+    return opperation_defined_name_and_symbol(as_raw_map(), other.inverse().as_raw_map());
+}
+opperation_defined_name_and_symbol::opperation_defined_name_and_symbol( const std::map<user_defined_name_and_symbol, float> &m1,  const std::map<user_defined_name_and_symbol, float> &m2){
+    for(const auto &i: m1) map.emplace(i.first, i.second+ (m2.contains(i.first) ? m1.at(i.first) : 0) );
+    for(const auto &i: m2) map.emplace(i.first, i.second+ (m1.contains(i.first) ? m2.at(i.first) : 0) );
+}
+
+inline string opperation_defined_name_and_symbol::name() const{
+    string res;
+    for(auto i=map.begin(); true;){
+        res+=i->first.name();
+        if(i->second!=1) res+="^(" + std::to_string(i->second) + ")";
+        ++i;
+        if(i==map.end()) break;
+        res+="*";
+    }
+    return res;
+}
+inline string opperation_defined_name_and_symbol::symbol() const{
+    string res;
+    for(auto i=map.begin(); true;){
+        res+=i->first.symbol();
+        if(i->second!=1) res+="^(" + std::to_string(i->second) + ")";
+        ++i;
+        if(i==map.end()) break;
+        res+="*";
+    }
     return res;
 }
--- math_geom_physics/magnitude.hpp
+++ math_geom_physics/magnitude.hpp
@@ -1,7 +1,10 @@
 #ifndef MAGNITUDE_250206
 #define MAGNITUDE_250206
 
+#include <iostream>
+
 #include <algorithm>
+#include <memory>
 
 #include <string>
 
@@ -9,38 +12,145 @@
 #include <array>
 
 #include <fstream>
+#include <map>
+#include <set>
 
-class magnitude{    // A data class
+#include <cassert>
+
+class magnitude;
+class measure_unit;
+class magnitudes_base;
+class measure_units_system;
+
+class opperation_defined_name_and_symbol;
+class user_defined_name_and_symbol;
+class named;
+
+class name_and_symbol{
     private:
-        class magnitudeSymbols{
-            public:
-                enum basicComponents{DISTANCE, MASS, TIME, CURRENT, TEMPERATURE, SUBSTANCE_AMOUNT, LUMINOSITY, ROTATION, nBasicComponents};
-                std::string basicMagnitudesSymbols[nBasicComponents];
-                std::string basicUnitsSymbols[nBasicComponents];
-
-                magnitudeSymbols();
-        };
-
-        static magnitudeSymbols symbols;
-
-        typedef std::array<char, magnitudeSymbols::nBasicComponents> basicComponentExponents;
-
-        basicComponentExponents exponents;
-
-        inline magnitude(): exponents() {};
-        inline magnitude(magnitudeSymbols::basicComponents basicIndex): exponents{0} { exponents[basicIndex]=1; };
-
-        inline bool operator <(const magnitude &m) const { return exponents<m.exponents; }
+        virtual std::map<user_defined_name_and_symbol, float> as_raw_map() const = 0;
 
     public:
-        static magnitude distance, mass, time, current, temperature, substanceAmount, luminosity, rotation; // Basic ones
-        static magnitude area, volume, density, velocity, acceleration, force, 
-        pressure, energy, power, frequency, 
-        charge, voltage, resistance, capacitance, 
-        inductance, momentum, angularVelocity, 
-        work, heat, entropy, impedance;
+        virtual std::string name() const = 0;
+        virtual std::string symbol() const = 0;
 
-        static magnitude electricField, magneticField, electricFlux, magneticFlux;
+        virtual opperation_defined_name_and_symbol square() const = 0;
+        virtual opperation_defined_name_and_symbol cube() const = 0;
+        virtual opperation_defined_name_and_symbol inverse() const = 0;
+
+        virtual opperation_defined_name_and_symbol operator *(const name_and_symbol &other) const;
+        virtual opperation_defined_name_and_symbol operator /(const name_and_symbol &other) const;
+
+        inline friend std::ostream &operator <<(std::ostream &os, const name_and_symbol &ns) { return os << ns.name() << ", " << ns.symbol(); }
+};
+class user_defined_name_and_symbol: public name_and_symbol{
+    private:
+        std::string name_;
+        std::string symbol_;
+
+        inline std::map<user_defined_name_and_symbol, float> as_raw_map() const { return {std::pair(*this, 1)}; }
+    
+    public:
+        inline user_defined_name_and_symbol(const std::string &name, const std::string &symbol): name_(name), symbol_(symbol) {};
+
+        inline user_defined_name_and_symbol(const user_defined_name_and_symbol &other) = default;
+
+        inline std::string name() const { return name_; }
+        inline std::string symbol() const { return symbol_; }
+
+        opperation_defined_name_and_symbol square() const;
+        opperation_defined_name_and_symbol cube() const;
+        opperation_defined_name_and_symbol inverse() const;
+
+        inline bool operator ==(const user_defined_name_and_symbol &other) const { return name_==other.name_ && symbol_==other.symbol_; }
+        inline bool operator <(const user_defined_name_and_symbol &other) const { return name_==other.name_ ? name_<other.name_ : symbol_<other.symbol_; }
+};
+class opperation_defined_name_and_symbol: public name_and_symbol{
+    private:
+        std::map<user_defined_name_and_symbol, float> map;
+
+        inline float get_exp(const user_defined_name_and_symbol &key) const { return map.contains(key) ? map.at(key) : 0 ; }
+        inline std::map<user_defined_name_and_symbol, float> as_raw_map() const { return map; }
+
+    public:
+        opperation_defined_name_and_symbol(std::map<user_defined_name_and_symbol, float> &&m): map(m) { assert(map.size()==1); }
+        opperation_defined_name_and_symbol(const std::map<user_defined_name_and_symbol, float> &m1, const std::map<user_defined_name_and_symbol, float> &m2);
+        
+        std::string name() const;
+        std::string symbol() const;
+
+        opperation_defined_name_and_symbol square() const;
+        opperation_defined_name_and_symbol cube() const;
+        opperation_defined_name_and_symbol inverse() const;
+
+        friend class name_and_symbol;
+        friend class user_defined_name_and_symbol;
+        friend class named;
+};
+
+class named{
+    private:
+        const std::shared_ptr<name_and_symbol> ns;
+
+    public:
+        inline named(const std::string &name, const std::string &symbol): ns(new user_defined_name_and_symbol(name, symbol)) {}
+        inline named(const opperation_defined_name_and_symbol &_ns): ns(new opperation_defined_name_and_symbol(_ns)) {}
+
+        inline std::string name() const { return ns->name(); }
+        inline std::string symbol() const { return ns->symbol(); }
+
+        inline friend std::ostream &operator <<(std::ostream &os, const named &n) { return os << n.ns->name() << ", " << n.ns->symbol(); }
+
+        inline const name_and_symbol &payload() const { return *ns; }
+};
+
+class measure_unit: public named{       // A data class
+    private:
+        const magnitude *measures_;
+
+        double international_system_equivalence;
+
+        measure_unit(double value, const magnitude *measures, const std::string &name, const std::string &symbol):
+        named(name, symbol), measures_(measures), international_system_equivalence(value) {}
+
+    public:
+        const magnitude &measures() const { return *measures_; }
+
+        inline bool operator <(const measure_unit &other) const{
+            assert(measures_==other.measures_);
+            return international_system_equivalence<other.international_system_equivalence;
+        }
+        
+        friend class magnitude;
+};
+class magnitude: public named{          // A data class
+    private:
+        enum basicComponents{DISTANCE, MASS, TIME, CURRENT, TEMPERATURE, SUBSTANCE_AMOUNT, LUMINOSITY, ROTATION, nBasicComponents};
+
+        std::array<char, nBasicComponents> exponents;
+        std::set<measure_unit> units;
+
+        inline void set_international_system_unit(const std::string &name, const std::string &symbol) { units.insert(std::move(measure_unit(1, this, name, symbol))); }
+
+        inline magnitude(basicComponents basicIndex, const std::string &magn_name, const std::string &magn_symbol, const std::string &unit_name, const std::string &unit_symbol): 
+        named(magn_name, magn_symbol), exponents{0} { exponents[basicIndex]=1; set_international_system_unit(unit_name, unit_symbol); }
+        
+        inline magnitude(const magnitude &m, const std::string &magn_name, const std::string &magn_symbol, const std::string &unit_name, const std::string &unit_symbol): 
+        named(magn_name, magn_symbol), exponents{m.exponents} { set_international_system_unit(unit_name, unit_symbol); }
+        
+        inline magnitude(const opperation_defined_name_and_symbol &_ns): named(_ns), exponents{0} {};
+
+        inline bool operator <(const magnitude &m) const { return exponents<m.exponents; }
+        
+    public:
+        static magnitude distance, mass, time, current, temperature, substanceAmount, luminosity, rotation; // Basic ones
+        static magnitude area, volume, velocity, frequency, angularVelocity, acceleration;                  // Distance and time
+        static magnitude density, force, pressure, energy, momentum, work, power, heat, entropy;            // Mass and temperature
+        static magnitude charge, voltage, resistance, inductance, capacitance, impedance;                   // Electricity
+        static magnitude electricField, magneticField, electricFlux, magneticFlux;                          // Vectorial ones
+    
+        inline magnitude(const magnitude &m) = default;
+        inline magnitude(const magnitude &m, const std::string &name, const std::string &symbol): named(name, symbol), exponents(m.exponents) {}
 
         magnitude operator *(const magnitude &m) const;
         magnitude operator /(const magnitude &m) const;
@@ -50,46 +160,10 @@ class magnitude{    // A data class
         magnitude square() const;
         magnitude cube() const;
         magnitude inverse() const;
-        
-        std::string units() const;
-        std::string symbol() const;
-
-        inline friend std::ostream &operator <<(std::ostream &os, const magnitude &m) { return os << m.symbol(); }
-
-        friend class scalarVariable;
 };
 
 inline magnitude square(const magnitude &m) { return m.square(); }
 inline magnitude cube(const magnitude &m) { return m.cube(); }
 inline magnitude inverse(const magnitude &m) { return m.inverse(); }
-
-class scalarVariable{
-    private:
-        const magnitude unit;
-        double value;
-
-    public:
-        inline scalarVariable(double x, magnitude m): value(x), unit(m) {}
-
-        inline friend std::ostream &operator <<(std::ostream &os, const scalarVariable &v) { return os << v.value << " " << v.unit.units(); }
-};
-
-template<unsigned int nDimensions> class vectorialVariable{
-    private:
-        const magnitude unit_;
-        std::array<double, nDimensions> components;
-
-        inline vectorialVariable(const magnitude &unit): unit_(unit) {}
-
-    public:
-        vectorialVariable operator +(const vectorialVariable &v) const;
-        vectorialVariable operator -(const vectorialVariable &v) const;
-        vectorialVariable operator *(const vectorialVariable &v) const;
-        vectorialVariable operator /(const vectorialVariable &v) const;
-        vectorialVariable operator -() const;
-
-        friend vectorialVariable<3> crossProduct(const vectorialVariable<3> &v1, const vectorialVariable<3> &v2);
-
-};
 
 #endif
