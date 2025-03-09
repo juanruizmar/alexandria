ec90823ac27aaa2f27d72c669314014acab9e0de
Juan <jruizmartos@gmail.com>
2025-03-09 14:28:36 +0100

    minor changes

--- matmaths/fitPolynomial.m
+++ matmaths/fitPolynomial.m
@@ -4,7 +4,7 @@
 % NEVER TESTED
 
 function [coefs, R_sqr] = fitPolynomial(x, y, degree)
-    fprintf("PAY ATTENTION! function [coefs, R_sqr] = fit_polynomial(x, y, degree) is now being used by the first time");
+    fprintf("PAY ATTENTION! function [coefs, R_sqr] = fitPolynomial(x, y, degree) is now being used by the first time");
 
     coefs=polyfit(x,y,degree);
     R_sqr = 1 - (sum((y - polyval(p, x)).^2) / sum((y - mean(y)).^2));
