91f5379200ed8c45a4f391739ae962caba56b79e
Juan <rmjuan6@gmail.com>
2025-03-17 16:41:21 +0100

    included a function for input-output (extracted from another project) and fixed bug in cyclics

---
+++ matmaths/io.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* scientific_double_to_str(double value, int precision){
+    int size = snprintf(NULL, 0, "%.*e", precision, value);
+    if(size<0) return NULL;
+
+    char* buffer = (char*)malloc(size+1);
+    if(!buffer) return NULL;
+
+    snprintf(buffer, size + 1, "%.*e", precision, value);
+
+    return buffer;
+}
