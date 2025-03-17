b95a52257b6d471d06232a5de8b653e5d8b50a19
Juan <rmjuan6@gmail.com>
2025-03-17 16:44:17 +0100

    included scientific notation string function, in C++

---
+++ matmaths/io.cpp
@@ -0,0 +1,7 @@
+#include <iomanip>
+
+std::string scientific_double_to_str(double value, int precision) {
+    std::ostringstream oss;
+    oss << std::scientific << std::setprecision(precision) << value;
+    return oss.str();
+}
