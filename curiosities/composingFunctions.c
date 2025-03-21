7b56aec9a20eff875cfd6235f4e0c0383e1f3225
Juan <rmjuan6@gmail.com>
2025-03-22 00:33:09 +0100

    bug fixed

--- curiosities/composingFunctions.c
+++ curiosities/composingFunctions.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // The following functions take a double and return a double:
 double square(double x) { return x * x; }
 double cube(double x) { return x * x * x; }
-double cubic_root(double x) { return cbrt(x); }
+double cubic_root(double x) { return cbrt(x); } 
 double sigmoid(double x) { return 1 / (1 + exp(-x)); }
 double exponential(double x) { return exp(x); }
 double logarithm(double x) { return log(x); }
@@ -29,26 +30,20 @@ double compose(double (*functions[])(double), int n, double x) {
 }
 
 // This function takes an array of characters identifying functions and returns an array of function pointers
-double **composition_functions(char functions[], int n) {
-    double **composed_functions = malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) composed_functions[i] = select_function(functions[i]);
-    return composed_functions;
-}
-
-// Free the memory allocated for the array of function pointers
-void free_composition_functions(double **composed_functions, int n) {
-    for (int i = 0; i < n; i++) free(composed_functions[i]);
-    free(composed_functions);
+double (**composition_functions(char functions[], int n))(double) {
+    double (**composed_functions)(double) = (double (**)(double))malloc(n * sizeof(double (*)(double)));
+        for (int i = 0; i < n; i++) composed_functions[i] = select_function(functions[i]);
+            return composed_functions;
 }
 
 int main() {
     // Here, a string of operands and a string of operators are used to calculate a composition of functions
     char functions[] = {'s', 'g', 'e', 'l'};
-    double (*result_functions[])(double) = composition_functions(functions, sizeof(functions) / sizeof(functions[0]));
+    double (**result_functions)(double) = composition_functions(functions, sizeof(functions) / sizeof(functions[0]));
     printf(
         "The result of the composition is: %f\n", 
-        compose(result_functions, sizeof(result_functions) / sizeof(result_functions[0]), 1.0)
+        compose(result_functions, sizeof(functions) / sizeof(functions[0]), 2.0)
     );
-    free_composition_functions(result_functions, sizeof(result_functions) / sizeof(result_functions[0]));
+    free(result_functions);
     return 0;
 }
