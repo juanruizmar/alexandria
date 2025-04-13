5f7c1ad118794260cb072636d13a3c70dc4b6fac
Juan <rmjuan6@gmail.com>
2025-04-13 22:47:54 +0200

    histogram adapted for work with alexandria containers

--- data_build/histogram.cpp
+++ data_build/histogram.cpp
@@ -1,5 +1,30 @@
 #include "histogram.hpp"
+#include "nlohmann/json.hpp"
 
-inline void histogram::display(std::ostream &os){
-    for(int i=0; i<n_segments(); ++i) os << frequencies_[i] << " " << min_+i*segment_length() << " " << max_-(n_segments()-i-1)*segment_length() << std::endl;
+using namespace std;
+
+void histogram::display(std::ostream &os){
+    os << "{\n";
+    os << "  \"min\": " << min_ << ",\n";
+    os << "  \"max\": " << max_ << ",\n";
+    os << "  \"frequencies\": [";
+    for(size_t i = 0; true; ++i) {
+        os << frequencies_[i];
+        if (i==n_segments() - 1) break;
+        os << ", ";
+    }
+    os << "],\n";
+    os << "  \"total\": " << total_ << "\n";
+    os << "}\n";
+}
+histogram::histogram(std::ifstream &is) {
+    nlohmann::json json_data;
+    is >> json_data;
+
+    min_ = json_data["min"];
+    max_ = json_data["max"];
+    total_ = json_data["total"];
+
+    frequencies_.resize(json_data["frequencies"].size());
+    for (size_t i = 0; i < frequencies_.size(); ++i) frequencies_[i] = json_data["frequencies"][i];
 }
--- data_build/histogram.hpp
+++ data_build/histogram.hpp
@@ -9,9 +9,12 @@
 #include <fstream>
 #include <string>
 
+#include <iostream>
+
 class histogram{
     private:
         double min_, max_;
+        std::size_t total_;
         std::vector<std::size_t> frequencies_;
 
     public:
@@ -19,19 +22,30 @@ class histogram{
         histogram(const container &values): 
         min_(std::numeric_limits<double>::infinity()), 
         max_(-std::numeric_limits<double>::infinity()), 
-        frequencies_(std::vector<std::size_t>(std::ceil(std::sqrt(static_cast<double>(values.size()))), 0)){
-            for(auto i: values){
-                if(i<min_) min_=i;
-                if(i>max_) max_=i;
+        total_(values.size()),
+        frequencies_(std::vector<std::size_t>(1+std::floor(std::sqrt(values.size())), 0)){
+            for(auto i=values.get_forward_reading_iterator(); !i.finished(); i.advance()){
+                if(i.payload() < min_) min_=i.payload();
+                if(i.payload() > max_) max_=i.payload();
             }
 
-            for(auto i: values) ++frequencies_[std::floor((i-min_)/segment_length())];
+            for(auto i=values.get_forward_reading_iterator(); !i.finished(); i.advance())
+                ++frequencies_[std::floor((i.payload() - min_) / segment_length() + 0.5)];
         }
+        histogram(std::ifstream &is);
 
         inline double min() const { return min_; }
         inline double max() const { return max_; }
-        inline double segment_length() const { return (max_-min_)/n_segments(); }
+        
+        inline double segment_length() const { return (max_-min_)/(frequencies_.size()-1); }
         inline std::size_t n_segments() const { return frequencies_.size(); }
+        inline std::size_t frequency(std::size_t i) const { return frequencies_[i]; }
+        inline std::size_t frequency(double x) const { return frequencies_[std::floor((x - min_) / segment_length() + 0.5)]; }
+        inline std::size_t total() const { return total_; }
+
+        inline std::pair<double, double> segment(std::size_t i) const {
+            return std::pair<double, double>(min_ + (i-0.5) * segment_length(), min_ + (i+0.5) * segment_length());
+        }
 
         void display(std::ostream &os);
 };
